ba2d95078b407a6746509afe045c121b05527e61
Raymond Takor <thanos@Raymonds-MacBook-Pro.local>
2020-03-11 16:56:51 -0700

    Creating initial test setup and test methods.

--- session-test.go
+++ session-test.go
@@ -1,1 +1,66 @@
 package mls
+
+import "testing"
+
+const noExcept int = 0xffffffff
+
+//
+//SessionTest type definition
+//
+type SessionTest struct {
+	CipherSuite CipherSuite
+	Scheme      SignatureScheme
+	GroupSize   int
+	SecretSize  int
+	numSessions int
+	GroupID     []byte `tls:"head=1"`
+	UserID      []byte `tls:"head=1"`
+	Sessions    []Session
+
+	identityPrivs  []SignaturePrivateKey
+	credentials    []Credential
+	initPrivs      []HPKEPrivateKey
+	clientInitKeys []ClientInitKey
+	states         []State
+}
+
+func newIdentityKey() (SignaturePrivateKey, error) {
+
+	return scheme.Generate()
+}
+
+func freshSecret() ([]byte, error) {
+
+	return getRandomBytes(secretSize)
+}
+
+func broadcastAdd() {
+
+}
+func broadcastAdd(from int, index int) {
+
+}
+
+func setupSession(t *testing.T) SessionTest {
+	sessionTest := SessionTest{}
+
+	sessionTest.CipherSuite = P256_SHA256_AES128GCM
+	sessionTest.Scheme = Ed25519
+	sessionTest.GroupSize = 5
+	sessionTest.SecretSize = 32
+	sessionTest.GroupID = []byte{0x01, 0x02, 0x03, 0x04}
+	sessionTest.UserID = []byte{0x04, 0x05, 0x06, 0x07}
+	sessionTest.numSessions = 1
+
+	for i := 0; i < numSession; i++ {
+
+		for j := 0; j < groupSize; j++ {
+
+			broadcastAdd()
+		}
+		//sessionTestSession.append()
+	}
+
+	//start(groupID []byte, myCIKs []ClientInitKey, otherCIKs []ClientInitKey, initSecret []byte) (*Session, *Welcome, error) {
+
+}
