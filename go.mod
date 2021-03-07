5d339a40c2b870757bd4ecf0ddf42f33a54585ec
Richard Barnes <rlb@ipv.sx>
2021-03-07 13:50:20 -0500

    Add test harness binary and initialize go module

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/cisco/go-mls/v0/mls
+
+go 1.16
+
+require (
+	github.com/mlswg/mls-implementations v0.0.0-20210304150611-54cdbd4604b4
+	google.golang.org/grpc v1.36.0
+)
---
+++ interop/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	pb "github.com/mlswg/mls-implementations/interop/proto"
+)
+
+var (
+	implementationName = "go-mls"
+)
+
+///
+/// Mock client implementation
+///
+type MockClient struct {
+	pb.MLSClientServer
+}
+
+func NewMockClient() *MockClient {
+	return &MockClient{}
+}
+
+func (mc *MockClient) Name(ctx context.Context, req *pb.NameRequest) (*pb.NameResponse, error) {
+	return &pb.NameResponse{Name: implementationName}, nil
+}
+
+func (mc *MockClient) SupportedCiphersuites(ctx context.Context, req *pb.SupportedCiphersuitesRequest) (*pb.SupportedCiphersuitesResponse, error) {
+	return nil, status.Error(codes.Unimplemented, "Method not implemented")
+}
+
+func (mc *MockClient) GenerateTestVector(ctx context.Context, req *pb.GenerateTestVectorRequest) (*pb.GenerateTestVectorResponse, error) {
+	switch req.TestVectorType {
+	default:
+		return nil, status.Error(codes.InvalidArgument, "Unsupported test vector type")
+	}
+}
+
+func (mc *MockClient) VerifyTestVector(ctx context.Context, req *pb.VerifyTestVectorRequest) (*pb.VerifyTestVectorResponse, error) {
+	switch req.TestVectorType {
+	default:
+		return nil, status.Error(codes.InvalidArgument, "Unsupported test vector type")
+	}
+}
+
+///
+/// Run the server
+///
+
+var (
+	portOpt int
+)
+
+func init() {
+	flag.IntVar(&portOpt, "port", 50001, "port to listen on")
+	flag.Parse()
+}
+
+func main() {
+	port := fmt.Sprintf(":%d", portOpt)
+	log.Printf("Listening on %s", port)
+
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterMLSClientServer(s, NewMockClient())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
